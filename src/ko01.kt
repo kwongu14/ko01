8b7ba3b5667fc1ea4af19ab81a77c140a278b159
kimkwongu <kwongu14@gmail.com>
2020-12-14 03:10:01 +0900

    코틀린 비교 연산자12월14일

---
+++ src/ko01.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val result = 4 + 3
+    println(result)
+
+    var a: Int = 10
+    var b: Int = 10
+    val result1 = ++a
+    val result2 = b++
+    println("result1: $result1")
+    println("result2: $result2")
+    println("a: $a")
+    println("b: $b")
+}
